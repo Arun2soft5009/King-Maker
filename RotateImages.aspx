2cf1fdf256e5ceb0a4d19247b3b6de5e8c9b0f1c
unknown <arunkumar@KK-ELEARNING-1.softlogic.com>
2012-09-13 10:14:09 +0530

    Initial commit

---
+++ RotateImages.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RotateImages.aspx.cs" Inherits="RotateImages" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+  <title>Rotate Images in ASP.Net</title>
+</head>
+<body>
+  <form id="HowToRotateImages" runat="server">
+    <asp:Image ID="ImageToRotate" runat="server" AlternateText="Image To Rotate" ImageUrl="~/images/OnlineSLA.jpg" />
+    <br />
+    <asp:Button ID="btnRotateImages" runat="server" OnClick="btnRotateImages_Click" Text="Rotate Image Using RotateFlip" />
+    <asp:Button ID="btnRotateImage1" runat="server" OnClick="btnRotateImage1_Click" Text="Rotate Image Using RotateTransform" />
+    <br />
+    <asp:Image ID="ImageAfterRotation" runat="server" AlternateText="Image After Rotation" />
+  </form>
+</body>
+</html> 
---
+++ RotateImages.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+public partial class RotateImages : System.Web.UI.Page
+{
+    String pathToImagesFolder = HttpContext.Current.Request.PhysicalApplicationPath + "images\\";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnRotateImages_Click(object sender, EventArgs e)
+    {
+        String imageToRotateFileName = "OnlineSLA.jpg";
+        String imageAfterRotationFileName = "ImageAfterRotation.jpg";
+
+        // Load 'Image To Rotate' into Memory
+        using (System.Drawing.Image imageToRotate = System.Drawing.Image.FromFile(pathToImagesFolder + imageToRotateFileName))
+        {
+            // Now Rotate this Image by 90 degree without flipping
+            // This method is used to 'Rotate Image' or 'Flip Image' or 'Rotate and Flip Image'
+            imageToRotate.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            // Now Save the Rotated Image on File System
+            imageToRotate.Save(pathToImagesFolder + imageAfterRotationFileName);
+        }
+        // Display the Rotated image
+        ImageAfterRotation.ImageUrl = "~/images/" + imageAfterRotationFileName;  
+    }
+    protected void btnRotateImage1_Click(object sender, EventArgs e)
+    {
+          String imageToRotateFileName = "OnlineSLA.jpg";
+        String imageAfterRotationFileName = "ImageAfterRotation.jpg";
+
+        // Load 'Image To Rotate' into Memory
+        using (System.Drawing.Image imageToRotate = System.Drawing.Image.FromFile(pathToImagesFolder + imageToRotateFileName))
+        {
+            // Now Rotate this Image by 90 degree without flipping
+            // This method is used to 'Rotate Image' or 'Flip Image' or 'Rotate and Flip Image'
+            imageToRotate.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            // Now Save the Rotated Image on File System
+            imageToRotate.Save(pathToImagesFolder + imageAfterRotationFileName);
+        }
+
+        // Display the Rotated image
+        ImageAfterRotation.ImageUrl = "~/images/" + imageAfterRotationFileName;  
+
+    }
+}
